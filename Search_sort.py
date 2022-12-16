5ce39dcb7dd38f281a11b5c4ec95b10f11d1228a
Tymofii Tarasenko <tarasenko.pn@ucu.edu.ua>
2022-12-16 10:56:26 +0200

    selection_sort function upd

--- Search_sort.py
+++ Search_sort.py
@@ -15,8 +15,20 @@ def binary_search(list_of_values, value):
 
 def selection_sort(lst):
     """
+    A function for sorting a list of values by selection method.
+    >>> selection_sort([1,4,2,5,3,6,4,7,5,8,6])
+    [1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 8]
     """
-    pass
+    def swap(list_1, elem1, elem2):
+        list_1[elem1], list_1[elem2] = list_1[elem2], list_1[elem1]
+        return list_1
+    for elem in range(len(lst)):
+        min_element = elem
+        for j in range(elem, len(lst)):
+            if lst[min_element] > lst[j]:
+                min_element = j
+            swap(lst, elem, min_element)
+    return lst
 
 def quick_sort(lst):
     """
